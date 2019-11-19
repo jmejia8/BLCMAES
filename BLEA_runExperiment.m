c6b118bc5eea69d3ea7dac88f49c81a5408c0571
Jesus Mejia <jesusmejded@gmail.com>
2019-11-19 12:39:58 -0600

    solving PMM test functions

--- BLEA_runExperiment.m
+++ BLEA_runExperiment.m
@@ -3,12 +3,13 @@
 % cd D:\soft\NBLEA
 % cd D:\soft\BLEAQ
 % cd 'D:\workspace\Computational Intelligence'
-
+addpath('BLEA_D/')
 pwd
 
-maxRuns = 20;
+maxRuns = 31;
 BI_list = [];
-BI_list = [BI_list;getBLOPinfo('SMD',1:12,5)];
+BI_list = [BI_list;getBLOPinfo('PMM',1:6,5)];
+BI_list = [BI_list;getBLOPinfo('PMM',1:6,10)];
 % BI_list = [BI_list;getBLOPinfo('TP',1:10)];
 % BI_list = [BI_list;getBLOPinfo('SMD',1:12,10)];
 % BI_list = [BI_list;getBLOPinfo('SMD',1:12,20)];
@@ -26,23 +27,23 @@ for BI = BI_list'
         % for gold mining problem
         BI.UmaxFEs = 1500;
         BI.UmaxImprFEs = 70;
-        BI.LmaxFEs = 150;
+        BI.LmaxFEs = 1500;
         BI.LmaxImprFEs = 15;
     elseif BI.dim == 5 || BI.dim == 6 || strcmp(BI.fn(1:2),'tp')
         % for generic benchmark test problem or decision making problem 
         BI.UmaxFEs = 2500;
         BI.UmaxImprFEs = 350;
-        BI.LmaxFEs = 250;
+        BI.LmaxFEs = 2500;
         BI.LmaxImprFEs = 25;
     elseif BI.dim == 10
         BI.UmaxFEs = 3500;
         BI.UmaxImprFEs = 500;
-        BI.LmaxFEs = 350;
+        BI.LmaxFEs = 3500;
         BI.LmaxImprFEs = 35;
     elseif BI.dim == 20
         BI.UmaxFEs = 5000;
         BI.UmaxImprFEs = 750;
-        BI.LmaxFEs = 500;
+        BI.LmaxFEs = 5000;
         BI.LmaxImprFEs = 50;
     else
         error('unknown dimensionality');
@@ -51,8 +52,8 @@ for BI = BI_list'
     BI.u_N = 50;
     BI.l_N = 50;
 
-% 	parfor runNo = 1:maxRuns
-    for runNo = 1:maxRuns
+	parfor runNo = 1:maxRuns
+    % for runNo = 1:31
 		tic;
 % 		ins = wapper(BI,true);
         ins = BLCMAES(BI);
@@ -61,7 +62,12 @@ for BI = BI_list'
 		ins.BI = BI;
 		ins.alg = algName;
 		ins.runTime = toc;
+
+        saveBLCMAESresult(ins, BI.fno);
         fprintf('%s %s #%d [%g,%g]\n', ins.alg, ins.BI.fn, ins.runNo, ins.UF, ins.LF);
+        ins
+        % n = norm( ins.LX - PMM_Psi(ins.UX, length(ins.LX), 4));
+        % n
 	end	
 end
 
